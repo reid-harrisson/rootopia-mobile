d1dadc5c2f9d4197feb7ee6de99a5a9ecc648af8
Reid Harrison <reid.al.harrisson@gmail.com>
2025-02-03 22:34:49 +0100

    feat: add layout and route

---
+++ src/components/layout/styles.tsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  background-color: black;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+`;
+
+export const Wrapper = styled.div`
+  width: 390px;
+  height: 844px;
+  background-color: white;
+  border-radius: 20px;
+`;
---
+++ src/consts/path.ts
@@ -0,0 +1,6 @@
+export const HOME = "/";
+export const DASHBORD = "/dashbord";
+export const LEADERBOARD = "/leaderboard";
+export const RACE = "/race";
+export const RULE = "/rule";
+export const HIGHLIGHT = "/highlight";
