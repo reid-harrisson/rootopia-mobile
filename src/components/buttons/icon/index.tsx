5d16014598de53b75bd5449328616a170a0c74d0
Reid Harrison <reid.al.harrisson@gmail.com>
2025-02-04 00:22:03 +0100

    feat: add home page

---
+++ src/components/buttons/icon/index.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const IconButton = styled.button`
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+
+  &:hover {
+    opacity: 0.8;
+  }
+
+  &:active {
+    opacity: 0.6;
+  }
+`;
---
+++ src/consts/colors.ts
@@ -0,0 +1,6 @@
+export const ONYX = "#3C3A40";
+export const KELLY_GREEN = "#51C330";
+export const RAJAH = "#FEA468";
+export const BRIGHT_TURQUOISE = "#18D7E5";
+export const CHINESE_BLACK = "#0D0E1D";
+export const CHINESE_WHITE = "#DCE8E5";
