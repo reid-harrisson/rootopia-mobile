6e01da2ce726f3331533a3a0cba515df33b5f690
Reid Harrison <reid.al.harrisson@gmail.com>
2025-02-04 13:23:46 +0100

    feat: add layout

--- src/components/layout/styles.tsx
+++ src/components/layout/styles.tsx
@@ -4,7 +4,6 @@ import { COLORS } from "../../consts";
 export const Container = styled.div`
   background-color: ${COLORS.CHINESE_BLACK};
   display: flex;
-  flex-direction: column;
   justify-content: center;
   align-items: center;
   position: absolute;
@@ -12,13 +11,39 @@ export const Container = styled.div`
   top: 0;
   right: 0;
   bottom: 0;
+  gap: 40px;
 `;
 
-export const Wrapper = styled.div`
-  width: 390px;
-  height: 844px;
+export const Wrapper = styled.div<{ width: string; height: string }>`
+  width: ${({ width }) => width}px;
+  height: ${({ height }) => height}px;
   border-radius: 40px;
   overflow: hidden;
   border: solid 5px #242835;
   box-shadow: 0px 0px 10px 0 white;
+`;
+
+export const Group = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+export const Link = styled.button`
+  border: none;
+  padding: none;
+  background: transparent;
+  color: ${COLORS.RAJAH};
+  text-shadow: 0px 3px 3px ${COLORS.CHINESE_BLACK}A0;
+  cursor: pointer;
+  font-size: 20px;
+  font-weight: 700;
+
+  &:hover {
+    opacity: 0.8;
+  }
+
+  &:active {
+    opacity: 0.6;
+  }
 `;
